19e3780a2f1f5e5d509bea063af3823eb88c4955
maggi86 <meaganwilliams407@gmail.com>
2022-10-03 16:29:41 +0200

    2/6

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+    class="bg-white text-grey-10"
+     bordered >
+      <q-toolbar class="constraint" >
+        <q-btn flat icon="eva-camera-outline"
+        size="18px" dense 
+        to="/camera" class="large-screen-only q-mr-sm" />
+        <q-separator spaced inset
+        class="large-screen-only" />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasagram
+        </q-toolbar-title>
+        <q-btn flat icon="eva-home-outline"
+        size="18px" dense 
+        to="/" class="large-screen-only" />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer 
+    class="bg-white"
+    bordered
+    >
+          <q-tabs class="text-grey-10"
+          active-color="primary"
+          indicator-color="transparent"
+      >
+        <q-route-tab to="/" icon="eva-home-outline" />
+        <q-route-tab to="/camera" icon="eva-camera-outline" />
+       
+      </q-tabs>
+          <!-- <q-toolbar-title class="text-center">copywright &copy;2022</q-toolbar-title> -->
+      </q-footer>
+
+    <q-page-container class="bg-grey-1" >
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-sm-min)
+    height:77px
+.q-toolbar_title
+  font-size:30px
+  @media (max-width: $breakpoint-xs-max)
+   text-align:center
+.q-footer
+  .q-tab_icon
+    font-size:30px
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-page class="constraint q-pa-md" >
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="#">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold" >meagan_williams</q-item-label>
+          <q-item-label caption>
+            At your boyfriend's house
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+      <q-card-section>
+        <div>Gold Gate Bridge</div>
+        <div class="text-caption text-grey">June 10 9:23PM</div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data(){
+    return{
+      posts: [
+        {
+          id:1,
+          caption:'Gold Gate Bridge',
+          date: 1591776655504,
+          location:'At your boyfriend/s house',
+          imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:2,
+          caption:'Gold Gate Bridge',
+          date: 1591776655504,
+          location:'At your boyfriend/s house',
+          imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:3,
+          caption:'Gold Gate Bridge',
+          date: 1591776655504,
+          location:'At your boyfriend/s house',
+          imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:4,
+          caption:'Gold Gate Bridge',
+          date: 1591776655504,
+          location:'At your boyfriend/s house',
+          imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
